75910c23ecdf325dbc13dad1865af9d43fd587e3
castlhoo <ksungho9991@gmail.com>
2024-10-02 13:44:32 +0900

    Intial Commit : java

---
+++ src/main/java/com/ce/fisa/Step20K8sApplication.java
@@ -0,0 +1,13 @@
+package com.ce.fisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step20K8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step20K8sApplication.class, args);
+	}
+
+}
